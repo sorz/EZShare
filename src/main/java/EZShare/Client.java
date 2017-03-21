87da4d75e62fa42ceb1004c99fd950e55acafe75
sorz <orz@sorz.org>
2017-03-22 01:02:27 +1100

    Initial commit

---
+++ src/main/java/EZShare/Client.java
@@ -0,0 +1,11 @@
+package EZShare;
+
+/**
+ * Entrance of client program.
+ * Created by xierch on 2017/3/22.
+ */
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Hello, client.");
+    }
+}
---
+++ src/main/java/EZShare/Server.java
@@ -0,0 +1,11 @@
+package EZShare;
+
+/**
+ * Entrance of server program.
+ * Created by xierch on 2017/3/22.
+ */
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("Hello, server.");
+    }
+}
